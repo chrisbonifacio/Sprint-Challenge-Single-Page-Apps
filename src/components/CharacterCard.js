2462bf75e8251a656617b824619a341811d11f0f
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-08-23 11:45:48 -0400

    successful api request, built CharacterList and CharacterCard components with state and props

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,36 @@
 import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  console.log(props);
+  const {
+    image,
+    name,
+    species,
+    status,
+    location,
+    origin,
+    episode
+  } = props.character;
+  return (
+    <Card>
+      <Image src={`${image}`} />
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>
+          <span className="date">{species}</span>
+        </Card.Meta>
+        <Card.Description>
+          <p>Location: {`${location.name}`}</p>
+          <p>Origin: {`${origin.name}`}</p>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+  );
 }
---
+++ src/components/EpisodeLIst.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function EpisodeList() {
+  const [episodes, setEpisodes] = useState([]);
+
+  return;
+}
