7b1fcd5f9884d1369bebbb738af4544e594e1f68
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-08-23 12:16:21 -0400

    finished building EpisodeCard component

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Card, Icon } from "semantic-ui-react";
 
 const EpisodeCard = props => {
-  const { name, air_date, characters } = props.episode;
+  const { name, air_date, episode, characters } = props.episode;
   return (
     <Card>
       <Card.Content header={`${name}`} />
-      <Card.Content description={`${air_date}`} />
+      <Card.Content>
+        <p>Air Date: {air_date}</p>
+        <p>Episode: {episode}</p>
+      </Card.Content>
       <Card.Content extra>
-        <Icon name="user" />4 Friends
+        <a>
+          <Icon name="user" />
+          Characters
+        </a>
       </Card.Content>
     </Card>
   );
